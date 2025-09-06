5445d7fb8d455ee85d4d9d12254a8c24cffa1532
Marcos Almeida <marcosideas@gmail.com>
2025-09-06 19:24:08 -0300

    Adiciona arquivo index.js para APIs

---
+++ api/index.js
@@ -0,0 +1,16 @@
+/**
+ * Exportação centralizada de todas as APIs
+ * Facilita a importação em outros arquivos
+ */
+
+const whatsappIntegration = require('./whatsapp-integration');
+const googleCalendarIntegration = require('./google-calendar-integration');
+const webhookRoutes = require('./webhook-routes');
+const auth = require('./auth');
+
+module.exports = {
+  whatsapp: whatsappIntegration,
+  googleCalendar: googleCalendarIntegration,
+  webhooks: webhookRoutes,
+  auth
+};
